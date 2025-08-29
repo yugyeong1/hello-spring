0ce0f230e05c38ea0fe9b46ff79c17958b6656f9
yugyeong1 <dbrud1032@gmail.com>
2025-08-29 17:29:04 +0900

    Initial commit

---
+++ src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
---
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,45 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        // 중복되면 안 됨
+        validateDuplicatedMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicatedMember(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent((m) -> {
+                            throw new IllegalStateException("이미 존재하는 회원입니다.");
+                    });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 한 명 조회
+     */
+
+    public Optional<Member> findOne(long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
